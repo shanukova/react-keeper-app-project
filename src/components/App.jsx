56127cdd388c823152606dd9dd104bf9e467936a
Victoria Shanukova <vshanukova@gmail.com>
2021-12-15 12:00:00 -0800

    do a spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,75 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note'
+// import React from 'react';
+// import Header from './Header';
+// import Footer from './Footer';
+// import Note from './Note';
+// import notes from '../notes'
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       {notes.map(note =>(
+//         <Note
+//           title={note.title}
+//           content={note.content}
+//         />
+//       ))}
+//       <Footer />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
   return (
-    <div>
-      <Header />
-      <Note
-        title="This is the title"
-        content="This is the content"
-      />
-      <Footer />
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default App;
