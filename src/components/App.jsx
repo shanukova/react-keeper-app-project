fe7203bd8db637c08e8f878b834c862436b728d9
Victoria Shanukova <vshanukova@gmail.com>
2021-12-20 13:11:43 -0800

    add delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,75 +1,99 @@
-// import React from 'react';
-// import Header from './Header';
-// import Footer from './Footer';
-// import Note from './Note';
+import React, {useState} from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
 // import notes from '../notes'
-
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       {notes.map(note =>(
-//         <Note
-//           title={note.title}
-//           content={note.content}
-//         />
-//       ))}
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App;
-
-import React, { useState } from "react";
+import CreateArea from './CreateArea';
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
 
-  function handleChange(event) {
-    const { name, value } = event.target;
+  const [notes, setNotes] = useState([]);
 
-    setContact(prevValue => {
-      return {
-        ...prevValue,
-        [name]: value
-      };
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote]
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id
+      });
     });
   }
 
   return (
-    <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          value={contact.fName}
-          placeholder="First Name"
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          value={contact.lName}
-          placeholder="Last Name"
-        />
-        <input
-          onChange={handleChange}
-          name="email"
-          value={contact.email}
-          placeholder="Email"
-        />
-        <button>Submit</button>
-      </form>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote}/>
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        )
+      })}
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [contact, setContact] = useState({
+//     fName: "",
+//     lName: "",
+//     email: ""
+//   });
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+
+//     setContact(prevValue => {
+//       return {
+//         ...prevValue,
+//         [name]: value
+//       };
+//     });
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>
+//         Hello {contact.fName} {contact.lName}
+//       </h1>
+//       <p>{contact.email}</p>
+//       <form>
+//         <input
+//           onChange={handleChange}
+//           name="fName"
+//           value={contact.fName}
+//           placeholder="First Name"
+//         />
+//         <input
+//           onChange={handleChange}
+//           name="lName"
+//           value={contact.lName}
+//           placeholder="Last Name"
+//         />
+//         <input
+//           onChange={handleChange}
+//           name="email"
+//           value={contact.email}
+//           placeholder="Email"
+//         />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
